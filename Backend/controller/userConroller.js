251ffb3c1d58ac113cc9b8597cc84463bfcb7185
aalbin <aalbin.tom@gmail.com>
2023-03-15 16:02:09 +0530

    Update userConroller.js

--- Backend/controller/userConroller.js
+++ Backend/controller/userConroller.js
@@ -20,7 +20,8 @@ const registerUser = asyncHandler(async (req, res) => {
               res.status(400)
             throw new Error("Invalid Email or Password")
       }
-
+console.log(userExists);
+console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
     } else {
         const user = await User.create({
             name, email, password
