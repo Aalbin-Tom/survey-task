36dcd4e1fca370e028276bf7caba68cf7db5e669
aalbin <70876784+Aalbin-Tom@users.noreply.github.com>
2023-03-22 14:44:42 +0530

    Update bottleneck.js

--- Backend/Middlewears/bottleneck.js
+++ Backend/Middlewears/bottleneck.js
@@ -7,5 +7,5 @@ const limiter = rateLimit({
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
-
+//a
 module.exports= limiter
